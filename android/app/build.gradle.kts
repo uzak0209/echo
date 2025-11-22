bfd5ba0f2c146128e5b917760c9980ff99237923
geo991513 <hibiki212@outlook.jp>
2025-11-22 14:26:37 +0900

    animation依存関係追加

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.animation.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
