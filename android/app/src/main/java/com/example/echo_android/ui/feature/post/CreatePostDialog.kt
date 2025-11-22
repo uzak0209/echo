846ee1b7b3f672f4a4e2d4f12c740c98a591d2ca
geo991513 <hibiki212@outlook.jp>
2025-11-22 14:27:58 +0900

    KeyBoardTypeの指定

--- android/app/src/main/java/com/example/echo_android/ui/feature/post/CreatePostDialog.kt
+++ android/app/src/main/java/com/example/echo_android/ui/feature/post/CreatePostDialog.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.AlertDialog
@@ -23,6 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -104,6 +107,10 @@ fun CreatePostDialog(
                     minLines = 4,
                     maxLines = 8,
                     enabled = !postState.isLoading,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Default
+                    ),
                     supportingText = {
                         Text(
                             text = "${content.length} / 500文字",
